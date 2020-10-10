a5126884a6d6d3f0d8cccffebb14d0022e0bf4fd
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-10 14:48:52 +0530

    [Anuj] UC1 tic tac board initialied

--- TicTacToe_Game/MainProgram.cs
+++ TicTacToe_Game/MainProgram.cs
@@ -2,11 +2,13 @@
 
 namespace TicTacToe_Game
 {
-    class Program
+    class MainProgram
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic tac toe game");
+            TicTacGameplay ticTac = new TicTacGameplay();
+            ticTac.UC1_TicTacBoard();
         }
     }
 }
---
+++ TicTacToe_Game/TicTacGameplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe_Game
+{
+    public class TicTacGameplay
+    {
+        public void UC1_TicTacBoard()
+        {
+            char[] tictac_board = new char[10];
+            Console.WriteLine("Tic Tac board initialised !"+ tictac_board[1]);
+
+        }
+    }
+}
