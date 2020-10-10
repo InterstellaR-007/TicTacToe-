490a7303b4b30f26d8a4c3b3a772f84a04478e56
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-10 15:21:23 +0530

    [Anuj] uc2 letter selection

--- TicTacToe_Game/MainProgram.cs
+++ TicTacToe_Game/MainProgram.cs
@@ -9,6 +9,11 @@ namespace TicTacToe_Game
             Console.WriteLine("Welcome to Tic tac toe game");
             TicTacGameplay ticTac = new TicTacGameplay();
             ticTac.UC1_TicTacBoard();
+
+            Console.WriteLine("Enter the Letter 'X' or 'O' :");
+            char player_Input = char.Parse(Console.ReadLine());
+
+            ticTac.UC2_LetterAssign(player_Input);
         }
     }
 }
--- TicTacToe_Game/TicTacGameplay.cs
+++ TicTacToe_Game/TicTacGameplay.cs
@@ -7,6 +7,10 @@ namespace TicTacToe_Game
     public class TicTacGameplay
     {
         char[] tictac_board;
+        char player_Input = ' ';
+        char computer_Input = ' ';
+        char[] LetterIndex = { 'X', 'O' };
+
         public void UC1_TicTacBoard()
         {
             tictac_board = new char[10];
@@ -15,6 +19,17 @@ namespace TicTacToe_Game
                 tictac_board[i] =  ' ';
             }
             Console.WriteLine("Tic Tac board initialised !");
+
+        }
+        public void UC2_LetterAssign(char player_Input)
+        {
+            this.player_Input = player_Input;
+            Random random = new Random();
+            int input_CompIndex = random.Next(0, 2);
+            computer_Input = LetterIndex[input_CompIndex];
+
+            Console.WriteLine("Player Selected Letter is : " + player_Input);
+            Console.WriteLine("Computer Selected Letter is : " + computer_Input);
 
         }
     }
